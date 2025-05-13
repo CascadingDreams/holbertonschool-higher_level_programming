cf584fd9c94ac97523d192040350fe960c494ce8
Sammy <myallio94@gmail.com>
2025-05-13 20:33:40 +1000

    task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     str_len = len(sentence)
-    first_c = sentence[0]
     if str_len == 0:
-        return None
+        return (str_len, None)
+    first_c = sentence[0]
     return (str_len, first_c)
