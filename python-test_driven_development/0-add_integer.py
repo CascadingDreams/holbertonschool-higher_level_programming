34f84aad7c77a3156f54a82172cedcf672a311eb
Sammy <myallio94@gmail.com>
2025-05-19 15:44:47 +1000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,17 +7,17 @@ Adds two integers.
 def add_integer(a, b=98):
     '''
     Adds two integers and returns the result.
-    
+
     Args: 
         a (int or float): first num to add
         b (int or float): second num to add
-        
+    
     Returns:
         int: Adding a and b
-    
+
     Raises:
         TypeError: if a and/or b not an int or float
-    '''
+'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
