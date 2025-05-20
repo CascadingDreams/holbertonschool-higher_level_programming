6ae879fba8dffe98a1bfbe2a6c6a547ce5f2d261
Sammy <myallio94@gmail.com>
2025-05-20 11:59:46 +1000

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,4 +1,38 @@
 #!/usr/bin/python3
+
 '''
-Module 
+Module initialise square size.
+defines a square by private instance attribute size
 '''
+
+
+class Square:
+    '''
+    class Square that defines a square.
+
+    Attributes:
+        __size: private value of square size
+
+    '''
+    def __init__(self, size=0):
+        '''
+            Initialise a new Square
+
+            Args:
+                size: size of square
+
+            Raises:
+                TypeError: size must be integer
+                ValueError: size must be >= 0
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if not (size >= 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''
+            Returns: the current square area
+        '''
+        return self.__size ** 2
