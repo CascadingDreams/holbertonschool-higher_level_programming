fa69c2fd444245593d692afc23a846441f9b897d
Sammy <myallio94@gmail.com>
2025-05-20 11:25:04 +1000

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Module for square
+An empty class that defines a square
+'''
+
+
+class Square():
+    '''
+    Defines an empty square class.
+    
+    Attributes:
+        empty
+    '''
