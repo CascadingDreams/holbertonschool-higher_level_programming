361dc37937eb37ada95a7a0f53483d0c390d6da5
Sammy <myallio94@gmail.com>
2025-05-20 11:44:01 +1000

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''
+Module initialise square size.
+defines a square by private instance attribute size
+'''
+
+class Square:
+    '''
+    class Square that defines a square.
+
+    Attributes:
+        __size: private value of square size
+
+    '''
+    def __init__(self, size):
+        '''
+            Initialise a new Square.
+
+            Args:
+                size: size of square
+        '''
+        self.__size = size
