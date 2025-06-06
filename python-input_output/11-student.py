0c52740fdf33cfef07b3e8c59874de3f6f0fefe5
Sammy <myallio94@gmail.com>
2025-06-06 12:48:40 +1000

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+'''Module to write class '''
+
+
+class Student:
+    '''Student class'''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Initialise student with:
+            first name
+            last name
+            age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictonary rep of Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for attribute_name in attrs:
+                if attribute_name in self.__dict__:
+                    new_dict[attribute_name] = self.__dict__[attribute_name]
+            return new_dict
+
+    def reload_from_json(self, json):
+        """updates all attributes of Student instance"""
+        for attribute_name in json:
+            self.__dict__[attribute_name] = json[attribute_name]
