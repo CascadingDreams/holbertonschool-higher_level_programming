0564e4588de828011f732e3e7344d9626dfb930a
Sammy <myallio94@gmail.com>
2025-07-23 15:18:08 +1000

    task4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+/*
+prints two arguments passed to it, in the following format: “ is ”
+*/
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+const result = `${firstArg} is ${secondArg}`;
+console.log(result);
