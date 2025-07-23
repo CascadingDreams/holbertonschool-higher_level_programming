609b8c23f9b30d36c245086089bc18c97381c683
Sammy <myallio94@gmail.com>
2025-07-23 14:57:33 +1000

    task 2

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+/*
+Prints the first argument passed
+*/
+
+const firstArg = process.argv[3];
+
+if (process.argv[3]) {
+    console.log(firstArg);
+} else {
+    console.log('No argument');
+}
