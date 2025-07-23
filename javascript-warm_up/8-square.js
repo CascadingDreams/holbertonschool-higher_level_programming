81ef3e55598d163be069a0980d6a699be480cf6b
Sammy <myallio94@gmail.com>
2025-07-23 15:49:12 +1000

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const numberofTimes = process.argv[2];
+const square = 'X';
+
+if (isNaN(numberofTimes)) {
+  console.log('Missing size');
+}
+for (let i = 0; i < numberofTimes; i++) {
+  console.log(square.repeat(numberofTimes));
+}
