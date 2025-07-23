bbdbfa25906cf974b5f98a685114634c50c9b93a
Sammy <myallio94@gmail.com>
2025-07-23 14:40:17 +1000

    task1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+Writes 3 lines to output
+*/
+const c = 'C is fun';
+const p = 'Python is cool';
+const js = 'JavaScript is amazing';
+
+console.log(c);
+console.log(p);
+console.log(js);
