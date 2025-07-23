7360a5c49ed940c5e71651ac927c65e8a82b71b6
Sammy <myallio94@gmail.com>
2025-07-23 15:40:23 +1000

    task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const numberofTimes = process.argv[2];
+const whatisFun = 'C is fun';
+
+if (isNaN(numberofTimes)) {
+  console.log('Missing number of occurences');
+}
+for (let i = 0; i < numberofTimes; i++) {
+  console.log(whatisFun);
+}
