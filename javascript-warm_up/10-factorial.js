18559db1b04e5dc215781e11f8e0859c438bd643
Sammy <myallio94@gmail.com>
2025-07-23 16:06:27 +1000

    task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+/* prints factorial of first arg */
+
+const n = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+if (isNaN(n)) {
+  console.log('1');
+} else {
+  console.log(factorial(n));
+}
