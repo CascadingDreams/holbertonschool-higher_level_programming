8023b63ee3aa6c209a97a50026f426c2ba604840
Sammy <myallio94@gmail.com>
2025-07-23 15:25:11 +1000

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+/*
+prints My number: <first argument converted in integer>
+*/
+
+const firstArg = process.argv[2];
+
+if (isNaN(firstArg)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(firstArg)}`);
+}
